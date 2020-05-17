de29402b10a604b11464c708923501ab15866d59
einobaka <61556425+einobaka@users.noreply.github.com>
2020-05-17 16:40:39 -0700

    added buttons for city list array

---
+++ assets/script.js
@@ -0,0 +1,42 @@
+// https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=YOUR_API_KEY
+
+// https://api.openweathermap.org/data/2.5/forecast?q={city name}&appid={your api key}
+
+// $.ajax({
+//     url: "/api/getWeather",
+//     data: {
+//       zipcode: 97201
+//     },
+//     success: function( result ) {
+//       $( "#weather-temp" ).html( "<strong>" + result + "</strong> degrees" );
+//     }
+//   });
+
+var cities = [];
+
+function fillCity() {
+    $("#cities").empty();
+
+    for (let i = 0; i < cities.length; i++) {
+        var cityButton = $("<li>")
+        cityButton.attr("id", cities[i]);
+        cityButton.text(cities[i]);
+        $("#cities").append(cityButton);
+    }
+};
+
+$("#search").on("click", function (e) {
+    e.preventDefault();
+    var cityInput = $("#city").val().trim();
+    console.log(cityInput);
+    cities.push(cityInput);
+    fillCity();
+});
+
+$("#city").on("click", function (e) {
+    $(this
+        
+        )
+
+});
+
