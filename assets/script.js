a78ee103aea430140ebcdcbeeaa6a539427a62b9
einobaka <61556425+einobaka@users.noreply.github.com>
2020-05-18 15:14:07 -0700

    added remove function for citySpan when refreshing results

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,7 @@
 var APIkey = "d1cbb5981d42bc71b129a89f7ff66db5";
 
+var dateToday = moment().format('l');
+
 // Prevent search without data entry in search bar
 $("#city").on("click", function () {
     $("#search").attr("disabled", null);
@@ -12,7 +14,7 @@ var city = {
 
 // Adding cities function
 function fillCity() {
-   
+
     // Empty city array
     $("#cities").empty();
     // Loop append for additional cities
@@ -28,6 +30,7 @@ function fillCity() {
         var selectCity = $(this).attr("city");
         console.log(selectCity)
         $("#city").val(selectCity)
+        $("#citySpan").remove();
         callWeather();
     });
 };
@@ -43,27 +46,41 @@ $("#search").on("click", function (e) {
     var cityInput = $(city.input).val().trim();
     console.log(cityInput);
     cities.push(cityInput);
+    $("#citySpan").remove();
     fillCity();
     callWeather();
 });
 
-
 function callWeather() {
     var cityInput = $(city.input).val().trim();
     // console.log(cityInput);
-    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityInput + "&appid=" + APIkey
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityInput + "&units=imperial&appid=" + APIkey
     $.ajax({
         url: queryURL,
         method: "GET"
     })
         .then(function (response) {
             console.log(response);
+            var citySpan = $("<span>");
+            var icon = $("<img>");
+
+            var cityName = response.city.name;
+            var iconBase = "http://openweathermap.org/img/wn/";
+            var iconResponse = response.list[0].weather[0].icon;
+
+            $("#current").append(citySpan)
+            $(citySpan).attr("id", "citySpan").text(cityName + " " + dateToday + " ").append(icon)
+            $(icon).attr("src", iconBase + iconResponse + ".png")
+
+            console.log(cityName)
+
             clearCity()
         });
 };
 
 function clearCity() {
     $("#city").val("");
+
 };
 
 
