0d3f860d27f450789ae9557000b1fafdc1f7923f
einobaka <61556425+einobaka@users.noreply.github.com>
2020-05-19 16:22:22 -0700

    added 5 day forecast stats

--- assets/script.js
+++ assets/script.js
@@ -72,6 +72,7 @@ function callWeather() {
             var windP = $("<p>");
             var uvSpan = $("<p>");
             var icon = $("<img>");
+            var forecast = $("<span>");
 
             var qLat = response.coord.lat;
             var qLon = response.coord.lon;
@@ -87,12 +88,13 @@ function callWeather() {
 
             $("#current").append(currentDiv);
             $(currentDiv).append(citySpan).attr("id", "cityDiv")
-            $(citySpan).attr("id", "citySpan").text(cityName + " " + dateToday + " ").append(icon).append("(" + cityCond + ")").append(tempP).append(humP).append(windP).append(uvSpan);
+            $(citySpan).attr("id", "citySpan").text(cityName + " " + dateToday + " ").append(icon).append("(" + cityCond + ")").append(tempP).append(humP).append(windP).append(uvSpan).append(forecast);
             $(icon).attr("src", iconBase + iconResponse + ".png")
             $(tempP).text("Temperature: " + cityTemp + " ºF");
             $(humP).text("Humidity: " + cityHum + "%");
             $(windP).text("Wind Speed: " + cityWind + " MPH")
             $(uvSpan).attr("id", "uv");
+            $(forecast).text("Five Day Forecast").attr("class", "text");
 
             // Query test link
             // https://api.openweathermap.org/data/2.5/forecast?q=tolleson&units=imperial&appid=d1cbb5981d42bc71b129a89f7ff66db5
@@ -104,6 +106,72 @@ function callWeather() {
                 method: "GET"
             })
                 .then(function (response) {
+
+                    var timeStamp = [
+                        response.list[5].dt,
+                        response.list[13].dt,
+                        response.list[21].dt,
+                        response.list[29].dt,
+                        response.list[37].dt,
+                    ];
+                    var resDesc = [
+                        response.list[5].weather[0].description,
+                        response.list[13].weather[0].description,
+                        response.list[21].weather[0].description,
+                        response.list[29].weather[0].description,
+                        response.list[37].weather[0].description,
+                    ];
+                    var resCond = [
+                        response.list[5].weather[0].icon,
+                        response.list[13].weather[0].icon,
+                        response.list[21].weather[0].icon,
+                        response.list[29].weather[0].icon,
+                        response.list[37].weather[0].icon,
+                    ];
+                    var resTemp = [
+                        response.list[5].main.temp,
+                        response.list[13].main.temp,
+                        response.list[21].main.temp,
+                        response.list[29].main.temp,
+                        response.list[37].main.temp,
+                    ];
+                    var resHum = [
+                        response.list[5].main.humidity,
+                        response.list[13].main.humidity,
+                        response.list[21].main.humidity,
+                        response.list[29].main.humidity,
+                        response.list[37].main.humidity,
+                    ];
+
+                    for (let i = 0; i < 5; i++) {
+                        var smallCol = $("<div>");
+                        var tempPS = $("<p>");
+                        var humPS = $("<p>");
+                        var iconS = $("<img>");
+
+                        var iconBase = "http://openweathermap.org/img/wn/";
+                        var iconResponse = resCond[i];
+                        var dt = timeStamp[i];
+                        var temp = resTemp[i];
+                        var cond = resDesc[i];
+                        var hum = resHum[i];
+
+                        $("#fiveday").append(smallCol);
+                        $(smallCol).attr("class", "smallWeather").attr("id", dt).append("(" + cond + ")").append(iconS).append(tempPS).append(humPS)
+                        $(iconS).attr("src", iconBase + iconResponse + ".png");
+                        $(tempPS).text("Temp: " + temp + "ºF");
+                        $(humPS).text("Humidity: " + hum + "%");
+
+
+
+
+                        // console.log(iconResponse);
+
+
+
+
+                    }
+
 
                 });
 
